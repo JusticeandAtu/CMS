3705a7b564649bc6276630c8f692279602fde880
JusticeandAtu <149971266+JusticeandAtu@users.noreply.github.com>
2023-12-22 07:41:58 +0200

    Add files via upload

--- cms.js
+++ cms.js
@@ -1,62 +1,51 @@
+async function updatePrices(service) {
+  const newPrice = document.getElementById(service).value;
 
-// Function to update prices in pricelist.js using GitHub API
-async function updatePrices(item) {
-    const newPrice = document.getElementById(item).value;
-  
-    // Replace with your GitHub information
-    const username = 'JusticeandAtu';
-    const repoName = 'reserve-page';
-    const filePath = 'assets/js/pricelist.js';
-    const token = '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';
-  
-    // Fetch the current data from GitHub
-    const apiUrl = `https://api.github.com/repos/${username}/${repoName}/contents/${filePath}`;
-  
-    const response = await fetch(apiUrl, {
-      headers: {
-        Authorization: `token ${token}`,
-      },
-    });
-  
-    const data = await response.json();
-    const currentContent = atob(data.content);
-  
-    // Update the price for the specified item
-    const updatedContent = currentContent.replace(
-      new RegExp(`(\\b${item}\\s*:\\s*'\\$)[^']*`),
-      `$1${newPrice}`
-    );
-  
-    // Encode the updated content
-    const encodedContent = btoa(updatedContent);
-  
-    // Prepare the payload for updating the file
-    const payload = {
-      message: `Update ${item} price via CMS`,
-      content: encodedContent,
-      sha: data.sha,
-    };
-  
-    // Update the content on GitHub
-    const updateResponse = await fetch(apiUrl, {
-      method: 'PUT',
-      headers: {
-        Authorization: `token ${token}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(payload),
-    });
-  
-    const updatedDataFromGitHub = await updateResponse.json();
-    confirm(`${item} price updated successfully:`, updatedDataFromGitHub);
-  
-    // Refresh the displayed prices
-    // displayPrices(updatedDataFromGitHub);
-  }
-  
-  // Function to display prices on the page
-//   function displayPrices(data) {
-//     // Your display logic here
-//     console.log('Display prices:', data);
-//   }
-  
+  // Fill in your GitHub information
+  const username = 'JusticeandAtu'; // Updated GitHub username
+  const repoName = 'reserve-page'; // Updated GitHub repository name
+  const filePath = 'assets/js/prices.js'; // Updated file path within the repository
+  const token = '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'; // Replace with your GitHub token
+
+  // Construct the GitHub API URL
+  const apiUrl = `https://api.github.com/repos/${username}/${repoName}/contents/${filePath}`;
+
+  // Fetch the current data from GitHub
+  const response = await fetch(apiUrl, {
+    headers: {
+      Authorization: `token ${token}`,
+    },
+  });
+
+  const data = await response.json();
+  const currentContent = atob(data.content);
+
+  // Update the price for the specified service
+  const updatedContent = currentContent.replace(
+    new RegExp(`(\\b${service}['"]?\\s*:\\s*)\\d+`),
+    `$1${newPrice}`
+  );
+
+  // Encode the updated content
+  const encodedContent = btoa(updatedContent);
+
+  // Prepare the payload for updating the file
+  const payload = {
+    message: `Update ${service} price via CMS`,
+    content: encodedContent,
+    sha: data.sha,
+  };
+
+  // Update the content on GitHub
+  const updateResponse = await fetch(apiUrl, {
+    method: 'PUT',
+    headers: {
+      Authorization: `token ${token}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  });
+
+  const updatedDataFromGitHub = await updateResponse.json();
+  alert(`${service} price updated successfully:`, updatedDataFromGitHub);
+} 
